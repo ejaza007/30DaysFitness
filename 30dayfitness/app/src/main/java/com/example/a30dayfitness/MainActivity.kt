3d5f4f5459e25b4b2d4f67a9335fdd3d38a057fd
Armaghan Ejaz <ejaza@lafayette.edu>
2023-07-31 00:26:32 +0500

    Update

--- 30dayfitness/app/src/main/java/com/example/a30dayfitness/MainActivity.kt
+++ 30dayfitness/app/src/main/java/com/example/a30dayfitness/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -29,9 +34,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import com.example.a30dayfitness.data.Exercise
 import com.example.a30dayfitness.ui.theme.FitnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,9 +68,46 @@ fun FitnessApp(){
 }
 
 @Composable
-fun FitnessItem(){
+fun FitnessItem(
+    exercise: Exercise,
+    modifier: Modifier = Modifier
+){
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = modifier
+    ){
+        Column (
+            modifier = modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+        )
+        
+        {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        dimensionResource(R.dimen.padding_small)
 
+                    )) {
 
+                FitnessIcon(exercise.imageResourceId)
+                FitnessInfo(exercise.name,exercise.day )
+                Spacer(Modifier.weight(1f))
+                FitnessItemButton(expanded = expanded, onClick = { expanded = !expanded })
+                
+                
+                    }
+            if(expanded){
+                FitnessDesc(fitnessDesc = )
+            }
+            
+    }
+
+}
 }
 
 @Composable
